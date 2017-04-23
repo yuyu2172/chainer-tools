ec9b1e254098cd0296a6c029901301e16850b770
Yusuke Niitani <yuyuniitani@gmail.com>
2017-04-23 22:25:21 +0900

    update

--- chainer_tools/utils.py
+++ chainer_tools/utils.py
@@ -7,4 +7,7 @@ def figure2array(f):
         f.canvas.tostring_rgb(), dtype=np.uint8, sep='')
     arr = arr.reshape(
         f.canvas.get_width_height()[::-1] + (3,))
+
+    # output CHW
+    arr = arr.transpose(2, 0, 1)
     return arr
